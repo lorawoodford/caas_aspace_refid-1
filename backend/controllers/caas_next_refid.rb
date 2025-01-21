f9682c701749de6d91ef279af6d569d8669a2088
Lora Woodford <lorawoodford@gmail.com>
2025-01-21 11:44:02 -0500

    [FEATURE] Add new endpoints (#17)

--- backend/controllers/caas_next_refid.rb
+++ backend/controllers/caas_next_refid.rb
@@ -20,4 +20,46 @@ class ArchivesSpaceService < Sinatra::Base
     json_response(:resource_id => params[:resource_id], :next_refid => incremented_id)
   end
 
+  Endpoint.get('/plugins/caas_next_refid/find_by_uri')
+    .description("Get the next_refid for a specific resource")
+    .params(["resource_uri", String, "The uri of the desired resource.", :required => "true"])
+    .permissions([:administer_system])
+    .returns([200, (:caas_next_refid)]) \
+  do
+    resource_id = JSONModel.parse_reference(params[:resource_uri])&.fetch(:id)
+    current_refid = CaasAspaceRefid.find(resource_id: resource_id)
+
+    if current_refid
+      json = CaasAspaceRefid.to_jsonmodel(current_refid.id)
+
+      json_response(json)
+    else
+      raise NotFoundException.new("CaasAspaceRefid was not found for resource #{params[:resource_id]}")
+    end
+  end
+
+  Endpoint.post('/plugins/caas_next_refid/set_by_uri')
+    .description("Manually set next ref_id for provided resource")
+    .params(["resource_uri", String, "The uri of the desired resource.", :required => "true"],
+            ["next_refid", Integer, :next_refid, :required => "true"])
+    .permissions([:administer_system])
+    .returns([200, :updated]) \
+  do
+    resource_id = JSONModel.parse_reference(params[:resource_uri])&.fetch(:id)
+    current_refid = CaasAspaceRefid.find(resource_id: resource_id)
+    new_refid = params[:next_refid]
+    if current_refid
+      if current_refid.next_refid >= params[:next_refid]
+        raise BadParamsException.new(next_refid: "Cannot set a next_refid that is less than or equal to the current refid: #{current_refid.next_refid}")
+      end
+      new_refid_record = current_refid.update(next_refid: new_refid)
+      json = CaasAspaceRefid.to_jsonmodel(new_refid_record.id)
+      handle_update(CaasAspaceRefid, current_refid.id, json)
+    else
+      CaasAspaceRefid.create_from_json(JSONModel(:caas_next_refid).from_hash({:resource_id => resource_id,
+                                                                              :next_refid => new_refid }))
+    end
+
+    json_response(:resource_uri => params[:resource_uri], :next_refid => new_refid)
+  end
 end
--- backend/spec/controller_caas_next_refid_spec.rb
+++ backend/spec/controller_caas_next_refid_spec.rb
@@ -1,24 +1,140 @@
 require 'spec_helper'
 
 describe 'CAAS ref id plugin' do
-  context 'when no resource_id is provided' do
-    it 'throws an error' do
-      post '/plugins/caas_next_refid', params = { }
+  describe 'POST /plugins/caas_next_refid' do
+    context 'when no resource_id is provided' do
+      it 'throws an error' do
+        post '/plugins/caas_next_refid', params = { }
 
-      expect(last_response).not_to be_ok
-      expect(last_response.status).to eq(400)
-      expect(last_response.body).to include('error')
+        expect(last_response).not_to be_ok
+        expect(last_response.status).to eq(400)
+        expect(last_response.body).to include('error')
+      end
+    end
+
+    context 'when a resource_id is provided' do
+      let(:resource) { create_resource }
+
+      it 'creates a next_refid' do
+        post '/plugins/caas_next_refid', params = { "resource_id" => resource.id}
+
+        expect(last_response).to be_ok
+        expect(last_response.status).to eq(200)
+      end
     end
   end
 
-  context 'when a resource_id is provided' do
-    let(:resource) { create_resource }
+  describe 'GET /plugins/caas_next_refid/find_by_uri' do
+    context 'when a bad resource_uri is provided' do
+      let(:resource_uri) { "/repositories/2/resources/#{rand(100)}" }
 
-    it 'creates a next_refid' do
-      post '/plugins/caas_next_refid', params = { "resource_id" => resource.id}
+      it 'throws an error' do
+        get "/plugins/caas_next_refid/find_by_uri?resource_uri=#{resource_uri}"
 
-      expect(last_response).to be_ok
-      expect(last_response.status).to eq(200)
+        expect(last_response).not_to be_ok
+        expect(last_response.status).to eq(404)
+        expect(last_response.body).to include('CaasAspaceRefid was not found')
+      end
+    end
+
+    context 'when an existing resource_uri is provided' do
+      let(:resource) { create_resource }
+
+      before do
+        post '/plugins/caas_next_refid', params = { resource_id: resource.id}
+      end
+
+      it 'returns the next_refid' do
+        get '/plugins/caas_next_refid/find_by_uri', params = { resource_uri: resource.uri }
+
+        expect(last_response).to be_ok
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to match(/"next_refid":1/)
+      end
+
+
+      context 'when a user without administer system permissions' do
+        before do
+          make_test_user('archivist')
+        end
+
+        it 'denies access' do
+          as_test_user('archivist') do
+            get '/plugins/caas_next_refid/find_by_uri', params = { resource_uri: resource.uri }
+
+            expect(last_response).not_to be_ok
+            expect(last_response.status).to eq(403)
+            expect(last_response.body).to match(/Access denied/)
+          end
+        end
+      end
+
+    end
+  end
+
+  describe 'POST /plugins/caas_next_refid/set_by_uri' do
+    context 'when a bad resource_uri is provided' do
+      let(:resource_uri) { "/repositories/2/resources/#{rand(100)}" }
+
+      it 'throws an error' do
+        post '/plugins/caas_next_refid/set_by_uri', params = { resource_uri: resource_uri,
+                                                               next_refid: 300 }
+
+        expect(last_response).not_to be_ok
+        expect(last_response.status).to eq(400)
+        expect(last_response.body).to match(/Database integrity constraint conflict/)
+      end
+    end
+
+    context 'when an existing resource_uri is provided' do
+      let(:resource) { create_resource }
+
+      before do
+        post '/plugins/caas_next_refid/set_by_uri', params = { resource_uri: resource.uri}
+      end
+
+      it 'updates the next_refid to the provided value' do
+        post "/plugins/caas_next_refid/set_by_uri", params = { resource_uri: resource.uri,
+                                                               next_refid: 300}
+
+        expect(last_response).to be_ok
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to match(/"next_refid":300/)
+      end
+
+      context 'when a next_refid less than the current ref_id is provided' do
+        before do
+          model = instance_double(CaasAspaceRefid)
+          allow(CaasAspaceRefid).to receive(:find).with(resource_id: resource.id).and_return(model)
+          allow(model).to receive(:next_refid).and_return(300)
+        end
+
+        it 'throws an error' do
+          post "/plugins/caas_next_refid/set_by_uri", params = { resource_uri: resource.uri,
+                                                                 next_refid: 1 }
+
+          expect(last_response).not_to be_ok
+          expect(last_response.status).to eq(400)
+          expect(last_response.body).to include('Cannot set a next_refid that is less than or equal to the current refid')
+        end
+      end
+
+      context 'when a user without administer system permissions' do
+        before do
+          make_test_user('archivist')
+        end
+
+        it 'denies access' do
+          as_test_user('archivist') do
+            post '/plugins/caas_next_refid/set_by_uri', params = { resource_uri: resource.uri,
+                                                                   next_refid: 300}
+
+            expect(last_response).not_to be_ok
+            expect(last_response.status).to eq(403)
+            expect(last_response.body).to match(/Access denied/)
+          end
+        end
+      end
     end
   end
 end
